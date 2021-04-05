2d342fdd87a4f21e3ee1ed0aa03f0aa0248ee43d
Vyacheslav Makarov <novapc@ukr.net>
2021-04-05 20:05:39 +0300

    fixed require from Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,9 @@
     use function cli\line;
     use function cli\prompt;
 
+function run()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    return (line("Hello, %s!", $name));
+}
