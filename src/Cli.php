3557123bf06b04ee5e8f7bcc9e34f7ac1af468a8
Makarov Vyacheslav <novapc@ukr.net>
2021-04-07 13:12:09 +0300

    fix old files

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@
     use function cli\line;
     use function cli\prompt;
 
-function run()
+function sayHello()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
