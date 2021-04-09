e8331c83c9c60fd94f3c146e320dd436b40a5eb2
novapc74 <novapc@ukr.net>
2021-04-09 21:59:19 +0300

    fixed games-gcd & Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -28,20 +28,8 @@ function dataGenerator()
     $arrData = [$randFirstNum, $randSecondNum, $randOperand, $randArray];
     return($arrData);
 }
-// function returnResult($arr)
-// {
-//     if ($arr[0] === 1) {
-//             return(line('Correct!'));
-//     } elseif (count($arr) === 3) {
-//             $firstAnswer = line("'%s' is wrong answer ;(. Correct answer was '%s'.", $arr[1], $arr[2]);
-//             $secondAnswer = line("Let's try again, %s!", $arr[0]);
-//             $answer = [$firstAnswer, $secondAnswer];
-//             return($answer);
-//     }
-//         return(line('Congratulations, %s!', $arr[0]));
-// }
 
-function returnResult($nameUser, $correctAnswer, $answerUser)
+function returnResult(string $nameUser, string $correctAnswer, string $answerUser)
 {
     if ($correctAnswer != $answerUser) {
         line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answerUser, $correctAnswer);
--- src/Games/gcd.php
+++ src/Games/gcd.php
@@ -20,6 +20,7 @@ function runGameGcd()
         $randNum = dataGenerator();
         line('Question: %s %s', $randNum[0], $randNum[1]);
         $randNum[0] >= $randNum[1] ? $count = $randNum[1] : $count = $randNum[0];
+        $correctAnswer = '';
         for ($i = 1; $i < $count + 1; $i++) {
             if (($randNum[0] % $i) === 0 && ($randNum[1] % $i === 0)) {
                 $correctAnswer = strval($i);
