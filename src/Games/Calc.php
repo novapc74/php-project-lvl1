61d540c07c84bc3785b2475072c26c66bca09641
Makarov Vyacheslav <novapc@ukr.net>
2021-04-17 16:39:20 +0300

    remake brain-calc

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,27 +6,19 @@ use function Brain\Games\Engine\runGame;
 
 use const Brain\Games\Engine\NUMBER_ROUNDS;
 
-function calc(): array
+function isCalc($randFirstNum, $randOperand, $randSecondNum): string
 {
-    $arrOperand = ['+', '-', '*'];
-    $randFirstNum = rand(0, 10);
-    $randSecondNum = rand(0, 10);
-    $randOperand = rand(0, 2);
-    $question = "Question: $randFirstNum $arrOperand[$randOperand] $randSecondNum";
-    $correctAnswer = [];
-    switch ($arrOperand[$randOperand]) {
+    switch ($randOperand) {
         case '+':
-            $correctAnswer = strval($randFirstNum + $randSecondNum);
+            return (strval($randFirstNum + $randSecondNum));
             break;
         case '-':
-            $correctAnswer = strval($randFirstNum - $randSecondNum);
+            return (strval($randFirstNum - $randSecondNum));
             break;
         case '*':
-            $correctAnswer = strval($randFirstNum * $randSecondNum);
+            return (strval($randFirstNum * $randSecondNum));
             break;
     }
-    $data = [$question, $correctAnswer];
-    return $data;
 }
 
 function runGameCalc(): void
@@ -34,7 +26,13 @@ function runGameCalc(): void
     $mainQuestion = 'What is the result of the expression?';
     $arrQwest = [];
     for ($i = 0; $i < NUMBER_ROUNDS; $i++) {
-        $arrQwest[] = calc();
+        $randFirstNum = rand(0, 10);
+        $randSecondNum = rand(0, 10);
+        $operands = ['+', '-', '*'];
+        $randOperand = array_rand($operands);
+        $question = "Question: $randFirstNum $operands[$randOperand] $randSecondNum";
+        $correctAnswer = isCalc($randFirstNum, $operands[$randOperand], $randSecondNum);
+        $arrQwest[] = [$question, $correctAnswer];
     }
     runGame($arrQwest, $mainQuestion);
 }
